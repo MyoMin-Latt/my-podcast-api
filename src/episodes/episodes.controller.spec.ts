3677cb3dee394902f37a1d7cc02a0baa80b45fd9
Myo Min Latt <myominlatt330@gmail.com>
2025-03-05 11:09:32 +0630

    ConfigModule > in episodes for useless/ Test with mockEpisodesService

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EpisodesController } from './episodes.controller';
+import { EpisodesService } from './episodes.service';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
 
+  const mockEpisodesService = {
+    findAll: async () => [{ id: 'id' }],
+    findFeaturedEpisodes: async () => [{ id: 'id' }],
+    findOne: async () => ({ id: 'id' }),
+    create: async () => ({ id: 'id' }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EpisodesController],
+      providers: [{ provide: EpisodesService, useValue: mockEpisodesService}]
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
@@ -14,5 +23,13 @@ describe('EpisodesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return correct response', async () => {
+      const episodeId = 'id';
+      const result = await controller.findOne(episodeId);
+      expect(result).toEqual({id: "id"})
+    });
   });
 });
--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,13 +1,11 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { CreateEpisodeDto } from './dto/create-episode.dto';
-import { ConfigService } from 'src/config/config.service';
 
 
 @Controller('episodes')
 export class EpisodesController {
     constructor(private episodesService: EpisodesService,
-        private configService: ConfigService,
     ) { }
 
     @Get()
