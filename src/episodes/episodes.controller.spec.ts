d979ac0105724a76dc866826041801b1b6f7cd12
Myo Min Latt <myominlatt330@gmail.com>
2025-03-05 11:21:03 +0630

    find one

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -29,36 +29,22 @@ describe('EpisodesController', () => {
   });
 
   describe('findOne', () => {
-    describe('when episode is found', () => {
-      const episodeId = 'id';
-      const mockResult = { id: episodeId, name: 'my episode' };
+    const episodeId = 'id';
+    const mockResult = { id: episodeId, name: 'my episode' };
 
-      beforeEach(() => {
-        mockFindOne.mockResolvedValue(mockResult);
-      });
-
-      it('should call the service with correct params', async () => {
-        await controller.findOne(episodeId);
-        expect(mockFindOne).toHaveBeenCalledWith(episodeId);
-      });
-
-      it('should return correct response', async () => {
-        const result = await controller.findOne(episodeId);
-        expect(result).toEqual(mockResult);
-      });
+    beforeEach(() => {
+      mockFindOne.mockResolvedValue(mockResult);
     });
 
-    describe('when episode is not found', () => {
-      const episodeId = 'id2';
-
-      beforeEach(() => {
-        mockFindOne.mockResolvedValue(null);
-      });
-
-      it('should throw an error', async () => {
-        await expect(controller.findOne(episodeId)).rejects.toThrow('Episode not found');
-      });
+    it('should call the service with correct params', async () => {
+      await controller.findOne(episodeId);
+      expect(mockFindOne).toHaveBeenCalledWith(episodeId);
     });
-    
+
+    it('should return correct response', async () => {
+      const result = await controller.findOne(episodeId);
+      console.log("result findOne", result);
+      expect(result).toEqual(mockResult);
+    });
   });
 });
